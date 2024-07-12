8cbff355c3917750372a4b29fa8064558734b0bb
Dmitry Boris <boris.dmitriy00@mail.ru>
2024-07-12 15:44:53 +0300

    feat: add first test for item

--- src/__test__/SpeciesList.test.tsx
+++ src/__test__/SpeciesList.test.tsx
@@ -5,7 +5,6 @@ import SpeciesList from '../components/SpeciesList';
 import { OneSpecie } from '../types/Types';
 
 describe('SpeciesList Component', () => {
-  // Mock data for our tests
   const mockSpecies: OneSpecie[] = [
     {
       name: 'Specie 1',
@@ -61,33 +60,26 @@ describe('SpeciesList Component', () => {
   ];
 
   it('renders the specified number of cards', () => {
-    // Render the SpeciesList component with mockSpecies as props
     render(
       <MemoryRouter>
         <SpeciesList species={mockSpecies} />
       </MemoryRouter>,
     );
 
-    // Find all elements with role 'link' (since each card is wrapped in a Link component)
     const cards = screen.getAllByRole('link');
 
-    // Assert that the number of rendered cards matches the number of mockSpecies
     expect(cards).toHaveLength(mockSpecies.length + 1);
   });
-
-  // Additional tests can be added here
 });
 
 it('displays an appropriate message if no cards are present', () => {
-  // Render the SpeciesList component with an empty array as props
   render(
     <MemoryRouter>
       <SpeciesList species={[]} />
     </MemoryRouter>,
   );
 
-  // Find the element with the text 'data not found'
   const message = screen.getByText(/data not found/i);
-  // Assert that the message is in the document
+
   expect(message).toBeInTheDocument();
 });
