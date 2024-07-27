ea35a4675b1f036e31d744e4caac441cd6bb7b21
Dmitry Boris <boris.dmitriy00@mail.ru>
2024-07-27 17:24:30 +0300

    feat: add tests for specie card

--- src/__test__/SpecieCard.test.tsx
+++ src/__test__/SpecieCard.test.tsx
@@ -1,53 +1,112 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import React from 'react';
+import { render } from '@testing-library/react';
+import {
+  MemoryRouter,
+  Route,
+  Routes,
+  useLocation,
+  useParams,
+} from 'react-router-dom';
 import '@testing-library/jest-dom';
-import { debug } from 'jest-preview';
-import SpecieCard from '../components/SpeciesList';
-import { OneSpecie } from '../types/Types';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { useGetOneSpecieQuery } from '../redux';
+import SpecieCard from '../components/SpecieCard';
+import ThemeContext from '../components/contex/ThemeContext';
+import currentPageSpeciesSlice from '../redux/currentPageSpeciesSlice';
+import selectedItemsSpeciesSlice from '../redux/selectedItemsSpeciesSlice';
 
-describe('SpecieCard Component', () => {
-  const mockSpecies: OneSpecie[] = [
-    {
-      name: 'Specie 1',
-      url: 'http://example.com/species/1/',
-      classification: '',
-      designation: '',
-      average_height: '',
-      skin_colors: '',
-      hair_colors: '',
-      eye_colors: '',
-      average_lifespan: '',
-      homeworld: '',
-      language: '',
+// Mock the hooks
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn(),
+  useLocation: jest.fn(),
+}));
+jest.mock('../redux', () => ({
+  useGetOneSpecieQuery: jest.fn(),
+}));
+
+const store = configureStore({
+  reducer: {
+    currentPageSpecies: currentPageSpeciesSlice,
+    selectedItemsSpecies: selectedItemsSpeciesSlice,
+  },
+});
+
+const renderWithProviders = (
+  component: React.ReactElement,
+  { darkTheme = false, initialEntries = ['/specie/1'] } = {},
+) =>
+  render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={initialEntries}>
+        <ThemeContext.Provider value={darkTheme}>
+          <Routes>
+            <Route path="/specie/:id" element={component} />
+          </Routes>
+        </ThemeContext.Provider>
+      </MemoryRouter>
+    </Provider>,
+  );
+
+describe('SpecieCard', () => {
+  beforeEach(() => {
+    (useParams as jest.Mock).mockReturnValue({ id: '1' });
+    (useLocation as jest.Mock).mockReturnValue({
+      search: '?query=example',
+    });
+  });
+
+  it('renders loader when data is loading', () => {
+    (useGetOneSpecieQuery as jest.Mock).mockReturnValue({
+      isLoading: true,
+      isFetching: false,
+      isError: false,
+      data: undefined,
+    });
+    const { getByTestId } = renderWithProviders(<SpecieCard />);
+    expect(getByTestId('card-loader')).toBeInTheDocument();
+  });
+
+  it('displays error message when there is an error', () => {
+    (useGetOneSpecieQuery as jest.Mock).mockReturnValue({
+      isLoading: false,
+      isFetching: false,
+      isError: true,
+      data: undefined,
+    });
+    const { getByText } = renderWithProviders(<SpecieCard />);
+    expect(getByText(/Data not found/i)).toBeInTheDocument();
+  });
+
+  it('renders specie information when data is available', () => {
+    const mockData = {
+      name: 'Human',
+      classification: 'Mammal',
+      designation: 'Sentient',
+      average_height: '180',
+      skin_colors: 'caucasian, black, asian, hispanic',
+      hair_colors: 'blonde, brown, black, red',
+      eye_colors: 'brown, blue, green, hazel',
+      average_lifespan: '120',
+      homeworld: 'Earth',
+      language: 'Galactic Basic',
       people: [],
       films: [],
       created: '',
       edited: '',
-    },
-  ];
+      url: 'http://swapi.dev/api/species/1/',
+    };
 
-  it('displays an appropriate message if no data', () => {
-    render(
-      <MemoryRouter>
-        <SpecieCard species={[]} />
-      </MemoryRouter>,
-    );
-    debug();
+    (useGetOneSpecieQuery as jest.Mock).mockReturnValue({
+      isLoading: false,
+      isFetching: false,
+      isError: false,
+      data: mockData,
+    });
 
-    const message = screen.getByText(/data not found/i);
-
-    expect(message).toBeInTheDocument();
-  });
-
-  it('correct data displayed', () => {
-    render(
-      <MemoryRouter>
-        <SpecieCard species={mockSpecies} />
-      </MemoryRouter>,
-    );
-    debug();
-
-    const message = screen.getByText(/Name: Specie 1/i);
-    expect(message).toBeInTheDocument();
+    const { getByText } = renderWithProviders(<SpecieCard />);
+    expect(getByText(/Name: Human/i)).toBeInTheDocument();
+    expect(getByText(/Classification: Mammal/i)).toBeInTheDocument();
   });
 });
